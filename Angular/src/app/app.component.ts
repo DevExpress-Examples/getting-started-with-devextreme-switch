195657876fda09307861d2941d24b9ede937420c
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 12:02:05 +0300

    chore(angular): ng update 18 → 19

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -3,9 +3,10 @@ import notify from 'devextreme/ui/notify';
 import { DxSwitchTypes } from 'devextreme-angular/ui/switch';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   switchValue = true;
